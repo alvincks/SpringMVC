b8ebd47030aa34036ae5dd9f27a3326f0fec5998
alvincks <alvincks727@gmail.com>
2016-05-31 23:55:46 +0800

    version 2 - home

--- src/main/java/com/mvc/web/controller/LoginController.java
+++ src/main/java/com/mvc/web/controller/LoginController.java
@@ -15,16 +15,17 @@ public class LoginController {
 	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public String displayLoginPage(Model model) {
 		User user = new User();
-		user.setUsername("Enter UserName");
 		model.addAttribute("user", user);
 	
 		return "login";
 	}
 	
 	@RequestMapping(value="/login", method = RequestMethod.POST)
-	public String displayPostLoginPage(@ModelAttribute("user222") User user) {
+	public String displayPostLoginPage(@ModelAttribute("user") User user) {
 		System.out.println("User name:" + user.getUsername());
 		System.out.println("password:" + user.getPassword());
 		return "redirect:greeting.html";
 	}
+	
+	
 }
